1bd52f3be483eab8ee734cfd839f18b6b1ab9102
roy belkind <belkind27@gmail.com>
2021-05-11 16:22:04 +0300

    add socket io and angular matirial

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FormsModule } from '@angular/forms';
+const socketioConfig: SocketIoConfig = {
+  url: 'http://localhost:4000',
+  options: {},
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    SocketIoModule.forRoot(socketioConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
