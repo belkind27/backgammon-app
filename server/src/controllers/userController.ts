084fe1adbfb4e4019a484b542acc9f57979272a8
Urip <uriperla12@gmail.com>
2021-05-23 22:57:05 +0300

    iii

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,4 +1,26 @@
 const mongoose =  require('mongoose');
-const User = require('./libs/dbmodels/user');
+import express from "express";
+import {  IUser , User } from "../libs/dbmodels/user";
+import jwt from "jsonwebtoken";
+import { JWT_KEY } from "../constants";
+
+
+const userController = express.Router();
+
+userController.use(express.json());
+
+userController.get("/find-all-users", async (req, res) => {
+    const users: Array<IUser> = await User.find();  
+    res.status(202).send(users);
+  });
+  
+
+
+
+userController.get("/find-user", async (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+ //   const id11 = jwt.decode(token).id ;
+    
+  });
 
 
