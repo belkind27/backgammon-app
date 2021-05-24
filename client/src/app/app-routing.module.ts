32690faf9c0acd45c15f84cb75865ecf588ea5f1
roy belkind <belkind27@gmail.com>
2021-05-24 18:29:15 +0300

    rename guard

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/guards/guards';
+import { AuthGuard } from './core/guards/auth.guard';
 import { GetJwtService } from './core/services/get-jwt.service';
 import { FindFriendsMainComponent } from './modules/find-friends-page/components/find-friends-main/find-friends-main.component';
 import { GameMainComponent } from './modules/game-page/components/game-main/game-main.component';
